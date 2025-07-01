5976591ac0deb717aed4aa5f06b17b4ed74e6046
PedroB97 <137570527+PedroB97@users.noreply.github.com>
2025-07-01 13:14:37 -0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from wtforms_sqlalchemy.fields import QuerySelectField # Importar QuerySelectField
 
 # Importar User para validações e QuerySelectField
-from .models.models import User
+from models.models import User
 
 # Função para obter a lista de usuários para o QuerySelectField
 def user_query():
@@ -60,41 +60,3 @@ class TaskForm(FlaskForm):
     )
     submit = SubmitField("Salvar Tarefa")
 
-# --- NOVOS FORMULÁRIOS PARA GERENCIAMENTO DE USUÁRIOS ---
-
-class UserForm(FlaskForm):
-    username = StringField("Nome de Usuário",
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField("Email",
-                        validators=[DataRequired(), Email()])
-    is_admin = BooleanField("Administrador") # Campo para definir se é admin
-    submit = SubmitField("Salvar Usuário")
-
-    def __init__(self, original_username=None, original_email=None, *args, **kwargs):
-        super(UserForm, self).__init__(*args, **kwargs)
-        self.original_username = original_username
-        self.original_email = original_email
-
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            user = User.query.filter_by(username=self.username.data).first()
-            if user:
-                raise ValidationError("Este nome de usuário já está em uso. Escolha outro.")
-
-    def validate_email(self, email):
-        if email.data != self.original_email:
-            user = User.query.filter_by(email=self.email.data).first()
-            if user:
-                raise ValidationError("Este email já está em uso. Escolha outro.")
-
-class CreateUserForm(UserForm):
-    password = PasswordField("Senha", validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField("Confirmar Senha",
-                                     validators=[DataRequired(), EqualTo("password", message="As senhas devem ser iguais.")])
-    submit = SubmitField("Criar Usuário")
-
-class UpdateUserPasswordForm(FlaskForm):
-    password = PasswordField("Nova Senha", validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField("Confirmar Nova Senha",
-                                     validators=[DataRequired(), EqualTo("password", message="As senhas devem ser iguais.")])
-    submit = SubmitField("Alterar Senha")
