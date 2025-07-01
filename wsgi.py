0bac9204e3c334d4ed123f25c4d9f180f4846cf8
PedroB97 <137570527+PedroB97@users.noreply.github.com>
2025-07-01 13:08:40 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,17 @@
-import sys
 import os
+import sys
 
+# Adiciona o diretório atual ao sys.path
 project_home = os.path.dirname(os.path.abspath(__file__))
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-from src import create_app
+# Importa a função create_app do __init__.py
+from __init__ import create_app
 
+# Cria a instância da aplicação Flask
 application = create_app()
+
+if __name__ == "__main__":
+    application.run()
+
