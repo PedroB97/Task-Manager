44b32b67de74f9fa0051895af8018978565ba61a
PedroB97 <137570527+PedroB97@users.noreply.github.com>
2025-07-01 15:21:49 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # Importar modelos aqui para evitar importação circular
-from src.models.models import Project, Task, Attachment
+from models.models import Project, Task, Attachment
+
